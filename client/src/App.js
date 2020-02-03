12428d19db802b6d09d8ca5528249d3f24e98591
sadjediz <broadkatsme@gmail.com>
2020-02-02 18:32:09 -0800

    added base create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  // Initialize state
+  state = { name: "", age: "" }
+
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(obj => this.setState({ name: obj.name, age: obj.age }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+        {console.log(this.state)}
+      </div>
+    );
+  }
+}
+
+export default App;
