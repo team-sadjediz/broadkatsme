c576d679ab86b0ad8a74df34e7781a07cf492659
at235am <at235am@gmail.com>
2020-02-09 23:07:59 -0800

    added form input functionality; altered fonts

--- client/src/components/new-room/new-room.component.jsx
+++ client/src/components/new-room/new-room.component.jsx
@@ -1,49 +1,116 @@
 import React from "react";
 
-// import FormInput from "../form-input/form-input.component";
-// import CustomButton from "../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import { ReactComponent as LeftArrowIcon } from "../../assets/icons/caret-left-solid.svg";
 import { ReactComponent as RightArrowIcon } from "../../assets/icons/caret-right-solid.svg";
 import { ReactComponent as PlusIcon } from "../../assets/icons/plus-solid.svg";
 
 import "./new-room.style.scss";
+import { render } from "react-dom";
 
-const NewRoom = () => (
-  <div className="newroom-container">
-    <div className="header gap">Create Room</div>
+class NewRoom extends React.Component {
+  constructor(props) {
+    super(props);
 
-    {/* <div className="room-title">sdfsdfdsf</div> */}
+    this.state = {
+      ui: "WrMNfw2Z3oU232Ztl8jTxuRTFmE2",
+      room_name: "",
+      tags: "",
+      room_size: "",
+      privacy: ""
+    };
+  }
 
-    {/* <FormInput
-      className="room-title gap"
-      name="room-title"
-      type="email"
-      label="room title"
-      value=""
-      required
-    /> */}
-    <div className="tags-container gap">Add Tags</div>
-    <div className="toggle-container">
-      <PlusIcon />
-      <div className="values">nsfw</div>
-    </div>
+  handleSubmit = event => {
+    event.preventDefault();
 
-    <div className="settings gap">Privacy</div>
-    <div className="toggle-container">
-      <LeftArrowIcon />
-      <div className="values">Private</div>
-      <RightArrowIcon />
-    </div>
+    this.setState({
+      uid: "",
+      room_name: "",
+      tags: "",
+      room_size: "",
+      privacy: ""
+    });
+  };
 
-    <div className="settings gap">Room Type</div>
-    <div className="toggle-container">
-      <LeftArrowIcon />
-      <div className="values">Theatre</div>
-      <RightArrowIcon />
-    </div>
-    {/* <CustomButton className="new-room-btn">Create Room</CustomButton> */}
-  </div>
-);
+  handleChange = event => {
+    const { value, name } = event.target;
+    console.log("a:", name, value);
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="newroom-container">
+        <div className="header gap">Create Room</div>
+
+        {/* <div className="room-title">sdfsdfdsf</div> */}
+        <form className="form-container" onSubmit={this.handleSubmit}>
+          <FormInput
+            className="room-title gap"
+            name="room_name"
+            handleChange={this.handleChange}
+            value={this.state.room_name}
+            label="room title"
+            required
+          />
+
+          <FormInput
+            className="room-title gap"
+            name="tags"
+            handleChange={this.handleChange}
+            value={this.state.tags}
+            label="tags"
+            required
+          />
+
+          <FormInput
+            className="room-title gap"
+            name="room_size"
+            handleChange={this.handleChange}
+            value={this.state.room_size}
+            label="room size"
+            required
+          />
+
+          <FormInput
+            className="room-title gap"
+            name="privacy"
+            handleChange={this.handleChange}
+            value={this.state.privacy}
+            label="privacy"
+            required
+          />
+          {/* 
+          <div className="tags-container gap">Add Tags</div>
+          <div className="toggle-container">
+            <PlusIcon />
+            <div className="values">nsfw</div>
+          </div>
+
+          <div className="settings gap">Privacy</div>
+          <div className="toggle-container">
+            <LeftArrowIcon />
+            <div className="values">Private</div>
+            <RightArrowIcon />
+          </div>
+
+          <div className="settings gap">Room Type</div>
+          <div className="toggle-container">
+            <LeftArrowIcon />
+            <div className="values">Theatre</div>
+            <RightArrowIcon />
+          </div> */}
+
+          <CustomButton className="new-room-btn" type="submit">
+            Create Room
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default NewRoom;
