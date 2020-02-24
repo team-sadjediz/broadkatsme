5a04253a9dcc97531f5f7723a1498e4642b7e34f
izzykid <loveizzydinh@gmail.com>
2020-02-23 22:07:07 -0800

    lobby-page populates the card components. need to add the button and route it properly

--- client/src/pages/lobby-page/data/data.js
+++ client/src/pages/lobby-page/data/data.js
@@ -2,51 +2,39 @@ const data = {
     "properties": [
         {
             "roomID": 1233,
-            "index": 0,
             "name": "Anime Room",
             "tags": "Animation, Action",
-            "picture": "https://cdn.vox-cdn.com/thumbor/PdZbmB57vB9M-oFxApdoaI3oPF0=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/65162399/ply0947_fall_reviews_2019_tv_anime.0.jpg",
-            "isOwner": false
+            "thumbnail": "https://cdn.vox-cdn.com/thumbor/PdZbmB57vB9M-oFxApdoaI3oPF0=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/65162399/ply0947_fall_reviews_2019_tv_anime.0.jpg"
         },
         {
             "roomID": 545,
-            "index": 1,
             "name": "The Office",
             "tags": "Comedy",
-            "picture": "https://img.nbc.com/sites/nbcunbc/files/images/2016/1/19/MDot-TheOffice-640x360-MP.jpg",
-            "isOwner": true
+            "thumbnail": "https://img.nbc.com/sites/nbcunbc/files/images/2016/1/19/MDot-TheOffice-640x360-MP.jpg"
         },
         {
             "roomID": 656,
-            "index": 2,
             "name": "Twitch Stream",
             "tags": "Misc",
-            "picture": "https://securingtomorrow.mcafee.com/wp-content/uploads/2018/11/twitch.jpg",
-            "isOwner": false
+            "thumbnail": "https://securingtomorrow.mcafee.com/wp-content/uploads/2018/11/twitch.jpg"
         },
         {
             "roomID": 562,
-            "index": 3,
             "name": "Jonas Brothers Marathon",
             "tags": "Music",
-            "picture": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kevin-1559319020.png",
-            "isOwner": false
+            "thumbnail": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kevin-1559319020.png"
         },
         {
             "roomID": 876,
-            "index": 4,
             "name": "Family Guy",
             "tags": "Animation, TV Show",
-            "picture": "https://assets.change.org/photos/5/uf/vg/cBUfVGBNGKfVaJt-1600x900-noPad.jpg?1517447658",
-            "isOwner": false
+            "thumbnail": "https://assets.change.org/photos/5/uf/vg/cBUfVGBNGKfVaJt-1600x900-noPad.jpg?1517447658"
         },
         {
             "roomID": 765,
-            "index": 5,
             "name": "Harry Potts",
             "tags": "Movie, Fantasy",
-            "picture": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bustle.com%2Fp%2Fthe-harry-potter-movies-wont-be-streaming-on-hbo-max-21095362&psig=AOvVaw3Eg_dhSCmceHaq1iCPXYHr&ust=1581275398260000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLj_gIXUwucCFQAAAAAdAAAAABAE",
-            "isOwner": false
+            "thumbnail": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bustle.com%2Fp%2Fthe-harry-potter-movies-wont-be-streaming-on-hbo-max-21095362&psig=AOvVaw3Eg_dhSCmceHaq1iCPXYHr&ust=1581275398260000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLj_gIXUwucCFQAAAAAdAAAAABAE"
         }
     ]
 }
--- client/src/pages/lobby-page/lobby-page.component.jsx
+++ client/src/pages/lobby-page/lobby-page.component.jsx
@@ -8,30 +8,29 @@
 
 import React, {Component} from "react";
 import Card from "../../components/card/card.component";
-import data from "./data/data"
-
+import data from "./data/data.js"
+import axios from "axios";
 import BackArrowIcon from "../../assets/icons/back-arrow.svg";
 import NextArrowIcon from "../../assets/icons/next-arrow.svg";
 
-import Sidebar from "../../components/sidebar/sidebar.component";
-import CollapsibleContent from "../../components/collapsible-content/collapsible-content.component";
+import "./lobby-page.style.scss";
 
-import "./lobby-page.styles.scss";
-
-class LobbyPage extends Component{
+class LobbyPage extends React.Component{
  constructor(props){
     super(props);
     this.state = {
+    uid: "HIkChzFouMOC39Y0bLB0BTM4hte2",
     properties: data.properties, 
     property: data.properties[0]
     }
  }
-
+ 
     nextProperty = () => {
         const newIndex = this.state.property.index+1;
         this.setState({
             property: data.properties[newIndex]
         })
+        
     }
 
     prevProperty = () => {
@@ -40,42 +39,40 @@ class LobbyPage extends Component{
             property: data.properties[newIndex]
         })
     }
-
+    componentDidMount() {
+        axios
+        .get("http://localhost:5000/api/home/users-rooms?uid=" + this.state.uid)
+        .then(rooms => console.log("I have the user"))
+        .catch(error => {
+            console.error(error);
+            console.log("oof");
+        });
+    }
     render() {
-        const {properties, property} = this.state;
-        const slide = { 
-            transform: `translateX(${-property.index*(100/properties.length)}%)` 
-        };
+        // const {properties, property} = this.state;
+        // const slide = { 
+        //     transform: `translateX(${-property.index*(100/properties.length)}%)` 
+        // };
+
         return(
-        <div className="bigger-container">
-            <Sidebar side="left" visible="false">
-                <CollapsibleContent
-                    id="collapsible-activity"
-                    label="Activity"
-                    content={"WHATEVER"}
-                />
-                <CollapsibleContent
-                    id="collapsible-friends"
-                    label="Friends"
-                    content={"WHATEVER2"}
-                />
-            </Sidebar>
             <div className="container">
-                <div id='active_header' className='header'>ACTIVE ROOMS</div>
                 <div className="active-container">
-                    <div className="cards-carousel-wrapper" style={slide}>
-                        {
-                            properties.map(property=> 
+                    <div id='active_header' className='header'>ACTIVE ROOMS</div>
+                    <div className="cards-carousel-wrapper">
+                    {
+                            this.state.properties.map(property=> 
                             <div className="zoom">
                                 <Card key={ property.roomID } property={ property }></Card>
+                                {/* <div className="room-name"> NAME: { property.name }</div>
+                                <div className="room-tags"> TAGS: { property.tags } </div> */}
                             </div>
                             )
                         }
                     </div>
                 </div>
                 <img className="back-btn" src="https://image.flaticon.com/icons/svg/126/126492.svg" onClick={
-                            () => this.prevProperty()} disabled={property.index === 0}></img>
-                <img className="next-btn" src="https://image.flaticon.com/icons/svg/126/126490.svg" disabled={property.index === data.properties.length-3}
+                            () => this.prevProperty()} disabled={this.state.property.index === 0}></img>
+                <img className="next-btn" src="https://image.flaticon.com/icons/svg/126/126490.svg" disabled={this.state.property.index === data.properties.length-3}
                  onClick={() => this.nextProperty()}></img>
                 {/* <button className='back-btn' onClick={() => this.prevProperty()} disabled={property.index === 0}>
                     Prev
@@ -87,17 +84,14 @@ class LobbyPage extends Component{
                     <div id="featured_header" className='header'>FEATURED ROOMS</div>
                     <div className="cards-grid">
                     {
-                        properties.map(property=> 
+                        this.state.properties.map(property=> 
                         <div className="zoom">
-                            {/* <p className='title'>{property.name}</p> */}
                             <Card key={ property.roomID } property={ property }></Card>
-                            {/* <p className='title'>Tags: {property.tags}</p> */}
                         </div>
                         )
                     }
                     </div>
                 </div>
-            </div>
             </div>
         );
     }
