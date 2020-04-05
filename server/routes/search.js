ddc32d86901863c60fc5194e354dbd813204474d
Jelli <julie.nhudo101@gmail.com>
2020-04-04 19:11:17 -0700

    Small changes

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,45 +3,6 @@ var router = express.Router();
 
 const Room = require("../models/room.model");
 
-// ---------------------------------------------------------- SEARCH BY NAME ----------------------------------------------------------
-
-// How To Use
-// axios.get(`${BASE_API_URL}/search/name/${name}`)
-// returns all documents matching name (as per convention)
-router.get("/name/:name", async function(req, res) {
-  let name = req.params.name;
-  await Room.find({ name: name }, function(error, room) {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(room);
-    }
-  });
-});
-
-// ---------------------------------------------------------- SEARCH BY TAGS ----------------------------------------------------------
-
-// How To Use
-// axios.get(`${BASE_API_URL}/search/tags`, null, { params: { tags: tags }})
-// returns all documents containing at least 2 of the send tags (as per convention)
-router.get("/tags", async function(req, res) {
-  let tags = req.query.tags;
-  await Room.aggregate([
-    { $match: { "tags.1": { $exists: true } } },
-    {
-      $redact: {
-        $cond: [
-          { $gte: [{ $size: { $setIntersection: ["$tags", tags] } }, 2] },
-          "$$KEEP",
-          "$$PRUNE"
-        ]
-      }
-    }
-  ])
-    .then(documents => res.send(documents))
-    .catch(error => res.status(400).send(error));
-});
-
 // ---------------------------------------------------------- SEARCH BY TAGS & NAME ----------------------------------------------------------
 
 // How To Use
