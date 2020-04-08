e5b68cf38aea6e9e4bda19cee91c97160697103d
izzykid <loveizzydinh@gmail.com>
2020-04-07 17:29:26 -0700

    added new package from material ui lab (its really big) and added more functionality and beautified user profile

--- client/src/components/message-pop/message-pop.component.jsx
+++ client/src/components/message-pop/message-pop.component.jsx
@@ -1,42 +1,81 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
-// import MuiAlert from '@material-ui/lab/Alert';
+import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 
 // function Alert(props) {
 //   return <MuiAlert elevation={6} variant="filled" {...props} />;
 // }
 
-const MessagePop = (pop, message, severity) => {
-    const [open, setOpen] = React.useState(pop);
-    const useStyles = makeStyles(theme => ({
-        root: {
-            width: '100%',
-            '& > * + *': {
-            marginTop: theme.spacing(2),
-            },
-        },
-        }));
+// const MessagePop = (pop, message, severity) => {
+//     const [open, setOpen] = React.useState(pop);
+//     // const useStyles = makeStyles(theme => ({
+//     //     root: {
+//     //         width: '100%',
+//     //         '& > * + *': {
+//     //         marginTop: theme.spacing(2),
+//     //         },
+//     //     },
+//     //     }));
         
         
-        const classes = useStyles();
+//     //     const classes = useStyles();
         
-        const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+//         const handleClose = (event, reason) => {
+//         if (reason === 'clickaway') {
+//             return;
+//         }
         
-        setOpen(false);
-        };
+//         setOpen(false);
+//         };
 
-  return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        {/* <Alert onClose={handleClose} severity={severity}> */}
-            {message}
-        {/* </Alert> */}
-    </Snackbar>
-  );
-}
+//   return (
+//     <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+//         <Alert onClose={handleClose} severity={severity}>
+//             {message}
+//         </Alert>
+//     </Snackbar>
+//   );
+// }
 
-export default MessagePop;
+// export default MessagePop;
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
+  
+  export default function MessagePop(props) {
+    const classes = useStyles();
+    // const [open, setOpen] = React.useState(false);
+  
+    // const handleClick = () => {
+    //   setOpen(props.pop);
+    // };
+  
+    // const handleClose = (event, reason) => {
+    //   if (reason === 'clickaway') {
+    //     return;
+    //   }
+  
+    //   setOpen(false);
+    // };
+  
+    return (
+      <div className={classes.root}>
+        <Snackbar open={props.pop} autoHideDuration={6000}>
+          <Alert severity="success">
+            {props.message}
+          </Alert>
+        </Snackbar>
+      </div>
+    );
+  }
--- client/src/pages/user-profile-page/user-profile-page.component.jsx
+++ client/src/pages/user-profile-page/user-profile-page.component.jsx
@@ -1,16 +1,3 @@
-//Styles
-import "./user-profile-page.style.scss";
-// import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import FormInput from "../../components/form-input/form-input.component";
-
-import EditIcon from '@material-ui/icons/Edit';
-import CheckIcon from '@material-ui/icons/Check';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
-
-// import EditIcon from '@material-ui/icons/Edit';
-import Button from '@material-ui/core/Button';
 
 // import React from 'react';
 import React, { Component } from "react";
@@ -18,6 +5,27 @@ import axios from 'axios';
 import { connect } from "react-redux";
 import { BASE_API_URL } from "../../utils";
 
+//Styles
+import "./user-profile-page.style.scss";
+// import { makeStyles } from '@material-ui/core/styles';
+
+//icons
+import EditIcon from '@material-ui/icons/Edit';
+import CheckIcon from '@material-ui/icons/Check';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import CloseIcon from '@material-ui/icons/Close';
+import Fab from '@material-ui/core/Fab';
+import IconButton from "@material-ui/core/IconButton";
+
+//components
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+import Tag from "../../components/tag/tag.component";
+import FormInput from "../../components/form-input/form-input.component";
+import Profile from "../../components/profile/profile.component";
+import Carousel from "../../components/carousel/carousel.component";
+import MessagePop from "../../components/message-pop/message-pop.component";
 
 
 class UserProfilePage extends React.Component {
@@ -32,12 +40,20 @@ class UserProfilePage extends React.Component {
             movies: "",
             websites: "",
             music: "",
-            photoURL: ""
+            photoURL: "",
+            subscribedRooms: "",
+            ownedRooms: "",
+            feedback: false,
+            feedbackMessage: "",
+            isEditing: false,
+            isUser: true,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleChangePhoto = this.handleChangePhoto.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
         this.handleUpload = this.handleUpload.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
     }
 
     componentDidMount() {
@@ -62,8 +78,26 @@ class UserProfilePage extends React.Component {
         .catch(error => {
             console.log("error: " + error);
         });
-    }
 
+        axios
+        .get(`${BASE_API_URL}/userprops/rooms/${this.state.uid}`)
+        .then(rooms => {
+          this.setState({ subscribedRooms: rooms.data });
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+        axios
+            .get(`${BASE_API_URL}/home/rooms/?size=8`)
+            .then(rooms => {
+            this.setState({ ownedRooms: rooms.data });
+            })
+            .catch(error => {
+            console.error(error);
+            });
+    }
+    
     handleSubmit(e) {
         // this.setState({username: e.target.value});
         const details = {
@@ -83,11 +117,22 @@ class UserProfilePage extends React.Component {
         {
             console.log("success: ");
             console.log(result);
+            this.setState({isEditing : false, feedback: true, feedbackMessage: "Updated Profile"});
         })
         .catch(error => {
             console.log("error: " + error);
         });
+
+        setTimeout(() => {
+            this.setState({feedback: false, feedbackMessage: ""})
+          }, 3000);
+
     }
+
+    handleCancel(e) {
+        this.setState({isEditing: false});
+    }
+
     handleChange = e => {
         // console.log(e);
         // this.setState({username: e.target.value});
@@ -129,99 +174,165 @@ class UserProfilePage extends React.Component {
           
     }
 
+    handleEditClick = e => {
+        this.setState({isEditing : true});
+    }
+
+
     render(){
         return(
             <div className="profile">
-                <div className="profile-picture">
-                    <img
-                     src={`${BASE_API_URL}/userprofile/get-photo?photoUrl=${this.state.photoURL}`}
-                    //  src={`${BASE_API_URL}/room/get-thumbnail?thumbnailUrl=default1.png`}
-                     />
-                </div>
-                <div className="profile-buttons">
-                    <Fab className="edit-button" color="primary" aria-label="edit">
+            <MessagePop pop={this.state.feedback} message={this.state.feedbackMessage}/>
+            <div className="profile-picture">
+                <img
+                src={`${BASE_API_URL}/userprofile/get-photo?photoUrl=${this.state.photoURL}`}
+                //  src={`${BASE_API_URL}/room/get-thumbnail?thumbnailUrl=default1.png`}
+                />
+            </div>
+            {!this.state.isEditing && 
+            <div className="username-title">
+                {this.state.username}
+                <div className="bio">{this.state.biography}</div>
+            </div>
+            }
+            <div className="profile-buttons">
+                {!this.state.isEditing && !this.state.isUser && 
+                    <Tooltip title='Send Friend Request' placement='right'>
+                    <Fab className="friend-button" color="primary" aria-label="edit" onClick={this.handleAddFriend}>
+                        <PersonAddIcon />
+                    </Fab>
+                    </Tooltip>
+                }
+                {!this.state.isEditing && this.state.isUser &&
+                    <Tooltip title='Edit Profile' placement='right'>
+                    <Fab className="edit-button" color="primary" aria-label="edit" onClick={this.handleEditClick}>
                         <EditIcon />
                     </Fab>
-                    <label htmlFor="upload" for="upload">
-                        <Fab
-                        className="upload-button"
-                        color="primary"
-                        >
-                            <AddAPhotoIcon />
-                        </Fab>
-                    </label>
-                    <input
-                        type="file"
-                        id="upload"
-                        name="profile_picture"
-                        accept="image/png, image/jpeg"
-                        onChange={this.handleUpload}
-                    ></input>
+                    </Tooltip>
+                }
+                {this.state.isEditing &&
+                <div>
+                <label htmlFor="upload" for="upload">
+                    <Fab className="upload-button" color="primary" component="span">
+                        <AddAPhotoIcon />
+                    </Fab>
+                </label>
+                <input
+                    type="file"
+                    id="upload"
+                    name="profile_picture"
+                    accept="image/png, image/jpeg"
+                    onChange={this.handleUpload}
+                />
                 </div>
-                <div className="profile-details">
-                    <form class="profile-details-form">
-
-                        <FormInput
-                        label="Username"
-                        name="username"
-                        value={this.state.username}
-                        handleChange={this.handleChange}
-                        />
-
-                        <FormInput
-                        label="Biography"
-                        name="biography"
-                        value={this.state.biography}
-                        handleChange={this.handleChange}
-                        />
-
-                        <FormInput
-                        label="Tags *NOT UPDATING YET"
-                        name="tags"
-                        value={this.state.tags}
-                        // handleChange={this.handleChange}
-                        />
-
-                        <FormInput
-                        label="Movies"
-                        name="movies"
-                        value={this.state.movies}
-                        handleChange={this.handleChange}
-                        />
-
-                        <FormInput
-                        label="Music"
-                        name="music"
-                        value={this.state.music}
-                        handleChange={this.handleChange}
-                        />
-
-                        <FormInput
-                        label="Websites"
-                        name="websites"
-                        value={this.state.websites}
-                        handleChange={this.handleChange}
-                        />
-
-                        {/* <FormInput
-                        label="Privacy"
-                        name="privacy"
-                        value={this.state.privacy}
-                        handleChange={this.handleChange}
-                        /> */}
-
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                            // className={classes.button}
-                            startIcon={<CheckIcon />}
-                            onClick={this.handleSubmit}
-                        >
-                        Update
-                        </Button>
-                    </form>
-                </div>
+                }
             </div>
+            {!this.state.isEditing &&
+                    <div className="profile-show">
+                            <div className="profile-favorites">
+                                <Profile
+                                movies={this.state.movies}
+                                music={this.state.music}
+                                website={this.state.websites}
+                                />
+                            </div>
+                            <div className="profile-biography">
+                                <div className="profile-tags">
+                                {this.state.tags.length !== 0 && this.state.tags.map((value, index) => {
+                                    return <Tag type="label" text={value} />;
+                                })}
+                                </div>
+                                
+                            </div>
+                            <div className="subscribed-rooms">
+                                <div className="subscribed-title">Subscribed Rooms</div>
+                                <Carousel
+                                    properties={this.state.subscribedRooms}
+                                />
+                            </div>
+                            <div className="owned-rooms">
+                            <div className="owned-title">Owned Rooms</div>
+                                <Carousel
+                                    properties={this.state.ownedRooms}
+                                />
+                            </div>
+                        {/* </div> */}
+                    </div>
+                    }
+
+                    {this.state.isEditing &&
+                        <form class="profile-details-form">
+                            <FormInput
+                            label="Username"
+                            name="username"
+                            value={this.state.username}
+                            handleChange={this.handleChange}
+                            />
+
+                            <FormInput
+                            label="Biography"
+                            name="biography"
+                            value={this.state.biography}
+                            handleChange={this.handleChange}
+                            />
+
+                            <FormInput
+                            label="Tags *NOT UPDATING YET"
+                            name="tags"
+                            value={this.state.tags}
+                            // handleChange={this.handleChange}
+                            />
+
+                            <FormInput
+                            label="Movies"
+                            name="movies"
+                            value={this.state.movies}
+                            handleChange={this.handleChange}
+                            />
+
+                            <FormInput
+                            label="Music"
+                            name="music"
+                            value={this.state.music}
+                            handleChange={this.handleChange}
+                            />
+
+                            <FormInput
+                            label="Websites"
+                            name="websites"
+                            value={this.state.websites}
+                            handleChange={this.handleChange}
+                            />
+
+                            {/* <FormInput
+                            label="Privacy"
+                            name="privacy"
+                            value={this.state.privacy}
+                            handleChange={this.handleChange}
+                            /> */}
+
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                // className={classes.button}
+                                startIcon={<CheckIcon />}
+                                onClick={this.handleSubmit}
+                            >
+                            Update
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                // className={classes.button}
+                                startIcon={<CloseIcon />}
+                                onClick={this.handleCancel}
+                            >
+                            Cancel
+                            </Button>
+                        </form>
+
+                    }
+                </div>
         )
     }
 }
